6a770ea0f03e059fc45da9d870438222e595992c
Mana-Tap <92272037+Mana-Tap@users.noreply.github.com>
2021-10-30 12:49:19 -0400

    using a while loop with a for loops to complete the third hurdle

--- reeborg's world challenge.py
+++ reeborg's world challenge.py
@@ -17,10 +17,38 @@ def corner_right():
 
 
 for step in range(6):
+    corner_left()
+    turn_right()
+    corner_right()
+    turn_left()
+
+# Go to the following website and copy/paste the code:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# Using a while loop with a for loop to complete the third hurdle
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def corner_left():
+    turn_left()
+    move()
+
+
+def corner_right():
+    move()
+    turn_right()
+    move()
+
+
+while not at_goal():
+    if wall_in_front() == True:
         corner_left()
         turn_right()
         corner_right()
         turn_left()
-
-# Go to the following website and copy/paste the code:
-# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+    else:
+        move()
