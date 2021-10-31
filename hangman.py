603a2270ec39273109869ad8e99690d269d9a63c
Mana-Tap <92272037+Mana-Tap@users.noreply.github.com>
2021-10-31 15:55:11 -0400

    commented out the testing code

--- hangman.py
+++ hangman.py
@@ -9,7 +9,7 @@ word_length = len(chosen_word)
 lives = 6
 print(logo)
 # Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+# print(f'Pssst, the solution is {chosen_word}.')
 
 # Create blanks
 display = []
