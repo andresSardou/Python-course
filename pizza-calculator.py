f7972f8ec99c69ff3244259244de89e882bd2dff
Mana-Tap <92272037+Mana-Tap@users.noreply.github.com>
2021-11-07 04:02:18 -0500

    added .lower() fucntion to input fields

--- pizza-calculator.py
+++ pizza-calculator.py
@@ -1,30 +1,30 @@
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L \n")
-add_pepperoni = input("Do you want pepperoni? Y or N \n")
-extra_cheese = input("Do you want extra cheese? Y or N \n")
+size = input("What size pizza do you want? S, M, or L \n").lower()
+add_pepperoni = input("Do you want pepperoni? Y or N \n").lower()
+extra_cheese = input("Do you want extra cheese? Y or N \n").lower()
 
 #Small Pizza
-if size == "S":
+if size == "s":
   bill = 15
-  if add_pepperoni == "Y":
+  if add_pepperoni == "y":
     bill += 2
-  if extra_cheese == "Y":
+  if extra_cheese == "y":
     bill += 1
 
 #Medium Pizza
-if size == "M":
+if size == "m":
   bill = 20
-  if add_pepperoni == "Y":
+  if add_pepperoni == "y":
     bill += 3
-  if extra_cheese == "Y":
+  if extra_cheese == "y":
     bill +=1
 
 #Large Pizza
-if size == "L":
+if size == "l":
   bill = 25
-  if add_pepperoni == "Y":
+  if add_pepperoni == "y":
     bill += 3
-  if extra_cheese == "Y":
+  if extra_cheese == "y":
     bill += 1
 
 print(f"Your final bill is: ${bill}.")
