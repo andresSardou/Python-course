0553e7125c616fc1736eece68338c5d51c18f97f
Andres Sardou <92272037+andresSardou@users.noreply.github.com>
2023-11-25 18:26:42 -0500

    Update average-score.py

--- average-score.py
+++ average-score.py
@@ -1,8 +1,6 @@
-# 🚨 Don't change the code below 👇
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆hig
 
 sum_of_students = 0
 number_of_students = 0
@@ -12,5 +10,3 @@ for height in student_heights:
 
 average = sum_of_students / number_of_students
 print(round(average))
-
-# Write your code below this row 👇
