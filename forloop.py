fa8565f5889c3d35bfa3b70d4f9ef0d2ee70b219
Andres Sardou <92272037+andresSardou@users.noreply.github.com>
2023-11-25 18:28:33 -0500

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -2,9 +2,6 @@ fruits = ["Apple", "Peach", "Orange"]
 for fruit in fruits:
     print(fruits)
 
-# the for loop is assigning the variable "fruit" to each item in the list and printing it out.
-# behind the scenes
-
 fruit = "Apple"
 fruit = "Peach"
 fruit = "Orange"
