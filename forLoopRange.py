1ff9e0517311b3edb833012045d16e85c19ba0b5
Andres Sardou <92272037+andresSardou@users.noreply.github.com>
2023-11-25 18:28:22 -0500

    Update forLoopRange.py

--- forLoopRange.py
+++ forLoopRange.py
@@ -1,27 +1,17 @@
 for number in range(1, 10):
     print(number)
 
-# The forloop and range function wont include the number 10
-# You need to write it like this if you want to include the number 10.
-
 for number in range(1, 11):
     print(number)
 
-# To count and skip by a number
-
 for number in range(1, 11, 3):
     print(number)
-
-# to count to 100 and add the number to the next one
 
 total = 0
 
 for number in range(1, 101):
     total += number
-
 print(total)
-
-# practice exercise 1: adding all the even numbers
 
 total_exercise = 0
 
